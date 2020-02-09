704113452de8d395d27828fcf0424beace4e8e10
m4rkito <marcelorap345@gmail.com>
2020-02-08 22:52:50 -0300

    Episódios ja assistidos serão marcado de verde

--- com_gui/downanime.py
+++ com_gui/downanime.py
@@ -2,14 +2,16 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 import re, requests, urllib3
 from bs4 import BeautifulSoup as bs
-import time, platform, threading, os
+import time, platform, threading, os, pathlib
+import json
+
 
 class DownAnime():
 
 	def __init__(self):
 
 		#instancia o RoboBrowser, seta o parser como html.parser para que analise o html
-		self.escolha_anime =  None
+		self.escolha_anime = None
 		self.completo = True
 		self.link = "https://www.superanimes.org/busca?parametro="
 		self.resultados = []
@@ -17,6 +19,29 @@ class DownAnime():
 		self.sistema = platform.system()
 		self.opcoes = Options()
 		self.opcoes.add_argument("--headless")
+		# Variavel que guarda o diretório home
+		self.home = str(pathlib.Path.home())
+		# Verifica se a pasta de configuração do DownAnime existe
+		if 'DownAnime_confs' not in os.listdir(self.home):
+			# Cria o diretório de configuração
+			os.mkdir(self.home + '/DownAnime_confs')
+			# Define o diretório de configuração
+			self.home += '/DownAnime_confs/'
+			# Cria o arquivo de configuração
+			with open(self.home + 'downEps.json', 'w') as file:
+
+				self.dados = {}
+				file.write(json.dumps({}))
+				file.close()
+
+		else:
+			# Define o diretório de configuração
+			self.home += '/DownAnime_confs/'
+			# Cria o arquivo de configuração
+			with open(self.home + 'downEps.json', 'r') as file:
+
+				self.dados = json.loads(file.read())
+				file.close()
 
 	#função para fazer pesquisa do anime
 	def pesquisar(self, nome):
@@ -24,6 +49,7 @@ class DownAnime():
 		#verifica qual o site pq a ideia e pesquisar em varios sites caso não ache o anime neste
 		if self.link == "https://www.superanimes.org/busca?parametro=":
 			
+			self.resultados = []
 			#tranforma os espaços em (+) para realizar a pesquisa
 			nome = nome.replace(" ", "+")
 			#pesquisa os animes com nome parecido
@@ -36,11 +62,10 @@ class DownAnime():
 			
 	def mostra_animes(self):
 
+		#mostra os resultados
+		for anime in self.resultados:
 
-			#mostra os resultados
-			for anime in self.resultados:
-
-				print(f"{anime['index']}--{anime['nome'].h1.text}")
+			print(f"{anime['index']}--{anime['nome'].h1.text}")
 
 	#pega todos os episódios disponiveis
 	def episodios(self):
@@ -50,6 +75,10 @@ class DownAnime():
 			self.conexao = requests.get(self.resultados[self.escolha_anime]["nome"].h1.a["href"])
 			#guarda o nome do anime
 			self.nome_anime = self.resultados[self.escolha_anime]["nome"].h1.text
+			if self.nome_anime not in self.dados:
+				
+				self.dados[self.nome_anime] = []
+
 			#guarda o link do anime
 			self.link_anime = self.conexao.url
 			#parser para busca os eps
@@ -59,21 +88,27 @@ class DownAnime():
 			numero_episodios = re.findall("[\d]+", numero_episodios.find("div").find("li").text)[0]
 			numero = int(numero_episodios)
 			#constroi uma lista com todos os episodios disponiveis
-			self.anime_episodios = [f"episodio-{episodio}" for episodio in range(1, numero+1)]
-			
+			self.anime_episodios = [f"episodio-{episodio}" for episodio in range(1, numero + 1)]
+			for index, ep in enumerate(self.anime_episodios):
 
+				if ep in self.dados[self.nome_anime]:
+
+					self.anime_episodios[index] += ';V'
+			
 	def mostra_episodios(self):
 		#mosta os episodios
 		for index, ep in enumerate(self.anime_episodios):
 				
 			print(f"{index} -- Episodio-{ep}")
 
-	
 	def baixar_ep(self, eps=[]):
 		try:
 
 			self.firefox = webdriver.Firefox(firefox_options=self.opcoes)
 			self.completo = False
+			for ep in eps:
+
+				self.dados[self.nome_anime].append(ep)
 
 			for ep in eps:
 				#abre a pagina do episodio
@@ -104,6 +139,7 @@ class DownAnime():
 						self.total += 1024
 				
 			self.firefox.close()
+			self.salvar_dados()
 			self.completo = True
 			
 		except KeyboardInterrupt:
@@ -112,7 +148,16 @@ class DownAnime():
 			self.firefox.close()
 			print("programa parado")
 
-	
+	def salvar_dados(self):
+		'''
+		Guarda quais episodios ja foram baixados
+		'''
+
+		with open(self.home + 'downEps.json', 'w') as file:
+
+			file.write(json.dumps(self.dados))
+			file.close()
+
 	def baixando(self):
 		
 		self.sistema = platform.system()
@@ -129,7 +174,7 @@ class DownAnime():
 			
 			print("baixando", end="", flush=True)
 
-			for c in [".",".",".","."]:
+			for c in [".", ".", ".", "."]:
 
 				print(c, end="", flush=True)
 				time.sleep(1)
@@ -142,5 +187,4 @@ class DownAnime():
 			elif self.sistema == "Windows":
 
 				os.system("cls")
-
 
